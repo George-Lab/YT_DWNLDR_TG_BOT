db7e1fa569292ca19661ece9b05427ccca28752c
George <me@example.com>
2024-05-30 15:52:36 +0300

    Added handler downloading and sending videos

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 Simple telegram bot for downloading videos from YT
 
 # Useful link for future developement
-    - [Полный гайд по разработке Telegram ботов с помощью Python](https://habr.com/ru/articles/732136/)
-    - [ytdlbot](https://github.com/tgbot-collection/ytdlbot/tree/master/ytdlbot)
+ - [Полный гайд по разработке Telegram ботов с помощью Python](https://habr.com/ru/articles/732136/)
+ - [ytdlbot](https://github.com/tgbot-collection/ytdlbot/tree/master/ytdlbot)
--- handlers.py
+++ handlers.py
@@ -1,16 +1,41 @@
 from aiogram import types, F, Router
-from aiogram.types import Message
+from aiogram.types import Message, FSInputFile
 from aiogram.filters import Command
+import logging
+import yt_dlp
+import os
 
 
 router = Router()
 
 
+# Download video using yt-dlp
+def download_video(url):
+    ydl_opts = {
+        "outtmpl": "downloaded_video.%(ext)s",
+        "format": "best",
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+
+# Command handler to start the bot
 @router.message(Command("start"))
-async def start_handler(msg: Message):
-    await msg.answer("Привет! Я помогу тебе узнать твой ID, просто отправь мне любое сообщение")
+async def start_handler(message: Message):
+    await message.answer("Hi!\nSend me a video link, and I'll download it for you!")
 
 
+# Message handler to process video links
 @router.message()
-async def message_handler(msg: Message):
-    await msg.answer(f"Твой ID: {msg.from_user.id}")
+async def message_handler(message: Message):
+    url = message.text
+    try:
+        await message.reply("Downloading the video, please wait...")
+        download_video(url)
+        video_file = "downloaded_video.mp4"
+        video_from_fs = FSInputFile(video_file)
+        result = await message.answer_video(video_from_fs, caption=video_file)
+        os.remove(video_file)  # Clean up the downloaded file
+    except Exception as e:
+        logging.error(f"Error: {e}")
+        await message.reply("Sorry, there was an error processing your request.")
